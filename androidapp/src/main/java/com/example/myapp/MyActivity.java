0a7f7fcdc0f76434e5e1ab6f0cc41b5d2e7ce0f7
IuliaGruia <gruia_iulia@yahoo.com>
2015-11-06 16:54:29 +0200

    am adaugat paginile de start/finish game; am adaugat validarea pentru cuvontele introduse deva;timer-ul si scorul

--- androidapp/src/main/java/com/example/myapp/MyActivity.java
+++ androidapp/src/main/java/com/example/myapp/MyActivity.java
@@ -2,11 +2,13 @@ package com.example.myapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,7 +29,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.Normalizer;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
+import android.os.CountDownTimer;
+import android.view.inputmethod.InputMethodManager;
+import org.w3c.dom.Text;
 
 public class MyActivity extends Activity {
 
@@ -36,6 +42,14 @@ public class MyActivity extends Activity {
     public static final String OUTPUT_MESSAGE_BAD = " is BAD";
 
     private TextView resultTextView;
+    private TextView timer;
+    private TextView score;
+    private final long startTime = 60000;
+    private final long interval = 1000;
+    private MalibuCountDownTimer countDownTimer;
+    private int numberOfLeters = 0;
+    private ArrayList<String> responseWords = new ArrayList<String>();
+    private boolean wordExistsInList = false;
 
     private class WordSearchThread extends Thread {
         private final String word;
@@ -83,10 +97,28 @@ public class MyActivity extends Activity {
         //but the redirect is good; the word exists
         private void printOutputMessage(String outputMessage, String responseWord) {
             if (responseWord.equals(word)) {
-                Log.i("Word " + word, " is OK");
-                resultTextView.setText(outputMessage + OUTPUT_MESSAGE_OK);
+
+                for (String s : responseWords) {
+                    if (s.equals(responseWord)) {
+                        wordExistsInList = true;
+                    }
+                }
+
+                if (!wordExistsInList) {
+                    responseWords.add(responseWord);
+                    Log.i("Word " + word, " is OK");
+                    resultTextView.setText(outputMessage + OUTPUT_MESSAGE_OK);
+                    numberOfLeters = numberOfLeters + responseWord.length();
+                    score.setText(" Score: " + numberOfLeters);
+                    wordExistsInList = false;
+
+                } else {
+                    resultTextView.setText("Please, try another word!");
+                    wordExistsInList = false;
+                }
             } else {
                 resultTextView.setText(outputMessage + OUTPUT_MESSAGE_BAD);
+                wordExistsInList = false;
             }
         }
 
@@ -142,23 +174,98 @@ public class MyActivity extends Activity {
 
     private class ButtonClickListener implements Button.OnClickListener {
 
+
         @Override
         public void onClick(View view) {
             EditText wordEntered = (EditText) findViewById(R.id.enter_word);
             Log.i("Word entered: ", wordEntered.getText().toString());
+            // Close virtual keyboard
+            InputMethodManager inputManager = (InputMethodManager)
+                    getSystemService(Context.INPUT_METHOD_SERVICE);
+
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+
 
             //call the thread to search the word
             new WordSearchThread(wordEntered.getText().toString()).start();
+
+            //clear textField
+            wordEntered.setText("");
         }
     }
+
+
+    private class ButtonClickListenerStartButton implements Button.OnClickListener {
+
+
+        @Override
+        public void onClick(View view) {
+            setContentView(R.layout.httpget);
+            resultTextView = (TextView) findViewById(R.id.result_text);
+            score = (TextView) findViewById(R.id.countPoints);
+            timer = (TextView) findViewById(R.id.timer);
+            countDownTimer = new MalibuCountDownTimer(startTime, interval);
+            timer.setText(timer.getText() + String.valueOf(startTime));
+            initHttpGetFrame();
+            countDownTimer.start();
+
+
+        }
+    }
+
+    private class ButtonClickListenerResetButton implements Button.OnClickListener {
+
+
+        @Override
+        public void onClick(View view) {
+            setContentView(R.layout.httpget);
+
+            score = (TextView) findViewById(R.id.countPoints);
+            timer = (TextView) findViewById(R.id.timer);
+            countDownTimer = new MalibuCountDownTimer(startTime, interval);
+            timer.setText(timer.getText() + String.valueOf(startTime));
+            initHttpGetFrame();
+            countDownTimer.start();
+            numberOfLeters = 0;
+
+        }
+    }
+
+
+    // CountDownTimer class
+    public class MalibuCountDownTimer extends CountDownTimer {
+
+        public MalibuCountDownTimer(long startTime, long interval) {
+            super(startTime, interval);
+        }
+
+        @Override
+        public void onFinish() {
+            timer.setText("Time's up!");
+            //startActivity(new Intent(MyActivity.this,Pop.class));
+            setContentView(R.layout.popwindow);
+            TextView scoreView = (TextView) findViewById(R.id.scoreView);
+            scoreView.setText("Score: " + numberOfLeters);
+            initAppResetButton();
+
+
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            timer.setText("Time remaining:" + (int) (millisUntilFinished / 1000));
+
+
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.httpget);
-        resultTextView = (TextView) findViewById(R.id.result_text);
-
+        setContentView(R.layout.main);
         initApp();
     }
 
@@ -186,9 +293,50 @@ public class MyActivity extends Activity {
     }
 
     private void initApp() {
-        ButtonClickListener buttonClickListener = new ButtonClickListener();
-        Button submitButton = (Button) findViewById(R.id.submit_button);
+        ButtonClickListenerStartButton buttonClickListener = new ButtonClickListenerStartButton();
+        Button submitButton = (Button) findViewById(R.id.start_game);
         submitButton.setOnClickListener(buttonClickListener);
     }
 
+    private void initHttpGetFrame() {
+        score.setText(" Score: " + numberOfLeters);
+
+        ButtonClickListener buttonClickListener = new ButtonClickListener();
+        Button submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(buttonClickListener);
+
+    }
+
+    private void initAppResetButton() {
+        numberOfLeters = 0;
+        score.setText(" Score: " + numberOfLeters);
+
+        ButtonClickListenerStartButton buttonClickListener = new ButtonClickListenerStartButton();
+        Button submitButton = (Button) findViewById(R.id.restartButton);
+        submitButton.setOnClickListener(buttonClickListener);
+    }
+
+
+
+
+    private int calculateScore(String s) {
+        int nr = 0;
+        String commonChars = "";
+        String t = "xzhj";
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (t.indexOf(ch) != -1) {
+                commonChars = commonChars + ch;
+            }
+        }
+
+        if (commonChars.length() == 0) {
+            nr = s.length();
+        } else if (s.length() > 6) {
+            nr = s.length() + 3 * commonChars.length();
+
+        } else nr = s.length() + commonChars.length();
+
+        return nr;
+    }
 }
